89855c506bef3ae974bf1abeda97de911ae4e3fb
Mirii <grawirus@gmail.com>
2026-08-26 22:30:48 +0200

    add new stage

---
+++ p1-l6/s1.js
@@ -0,0 +1,7 @@
+console.log(typeof 'hello'); // string
+console.log(typeof 123);    // number
+console.log(typeof true);   // boolean
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // обрати внимание на эту строку!   // object
+console.log(typeof { name: 'Алиса' });  // string ошибся было object
+console.log(typeof NaN);    // boolean ошибся был number
