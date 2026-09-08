ce9518d2dbb9495ed3f3ec8fa587481688e4a684
Miri <grawirus@gmail.com>
2026-09-08 01:30:36 -0700

    Complete stage 3 of 5

--- p2-l10/s1.js
+++ p2-l10/s1.js
@@ -18,6 +18,23 @@ const workDays = {
     Козлова: 20,
 };
 
+let totalPayroll = 0;
+let totalOPV = 0;
+let totalIPN = 0;
+let totalNet = 0;
+let fullMounthCount = 0;
+
 for (let employee of employees) {
-    console.log(`${employee}: ${salaries[employee]}`);
+    const accrued = Math.round(salaries[employee] / WORK_DAYS_IN_MONTH * workDays[employee]);
+    const accruedOpv = Math.round(accrued * OPV_RATE);
+    const accruedIPN = Math.round((accrued - accruedOpv) * IPN_RATE);
+    const takeHomePay = Math.round(accrued - accruedOpv - accruedIPN);
+    console.log(`${employee}: ${salaries[employee]}, Accrued: ${accrued}, Accrued OPV: ${accruedOpv}, Accrued IPN: ${accruedIPN}, Take Home Pay: ${takeHomePay}`);
+
+    totalPayroll += accrued;
+    totalOPV += accruedOpv;
+    totalIPN += accruedIPN;
+    totalNet += takeHomePay;
+    fullMounthCount ? WORK_DAYS_IN_MONTH === workDays[employee] : fullMounthCount++;
 }
+
