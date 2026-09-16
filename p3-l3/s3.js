25dc3693cfe44d40bcf74eddcb9fd64d0e3a9e40
Mirii <grawirus@gmail.com>
2026-09-16 20:12:07 +0200

    v3

--- p3-l3/s3.js
+++ p3-l3/s3.js
@@ -7,12 +7,28 @@
 // Проверь: calcFinalPrice(10000, 10) — должно получиться: скидка 10%, затем НДС 12%
 
 // Твой код:
+const price = 10000;
+const discountPercent = 10;
 
 
 function calcDiscount(price, percent) {
-
+    let discountAmount = price * percent / 100 ;
+    return price - discountAmount;
 }
 
 function calcVat(price) {
-
+    return price * 0.12 + price;
 }
+
+function calcFinalPrice(price, discountPercent) {
+    const discountPrice = calcDiscount(price, discountPercent);
+    return discountPrice;
+}
+
+// calcDiscount(10000, 10000);
+
+// console.log(calcDiscount(10000, 10));
+// console.log(calcVat(10000));
+// console.log(discountPercent);
+console.log(discountPrice);
+console.log(calcFinalPrice());
